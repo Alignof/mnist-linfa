58b8cf261e5a17f42a2f85e3ab0731862b4c587c
Alingof <Alignof@outlook.com>
2021-11-25 14:23:30 +0900

    [update] remove unused import

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use linfa::traits::{Fit, Transformer};
-use linfa::{Dataset, DatasetBase};
-use linfa_reduction::Pca;
-use linfa_tsne::{Result, TSneParams};
+use linfa::DatasetBase;
+use linfa_tsne::Result;
 use mnist::{Mnist, MnistBuilder};
-use ndarray::{Array, ArrayBase, OwnedRepr, Dim};
+use ndarray::{ArrayBase, OwnedRepr, Dim};
 use std::{io::Write, process::Command};
 
 fn export_data(ds: DatasetBase<ArrayBase<OwnedRepr<f64>, Dim<[usize; 2]>>, ArrayBase<OwnedRepr<u8>, Dim<[usize; 2]>>>) {
@@ -28,7 +27,7 @@ fn export_data(ds: DatasetBase<ArrayBase<OwnedRepr<f64>, Dim<[usize; 2]>>, Array
 
 fn main() -> Result<()> {
     // use 50k samples from the MNIST dataset
-    let trn_size = 5000_usize;
+    let trn_size: usize = 5000;
     let (rows, cols) = (28, 28);
 
     println!("started");
@@ -45,15 +44,15 @@ fn main() -> Result<()> {
     println!("downloaded dataset");
 
     // create a dataset from it
-    let ds = Dataset::new(
-        Array::from_shape_vec((trn_size, rows * cols), trn_img)?.mapv(|x| (x as f64) / 255.),
-        Array::from_shape_vec((trn_size, 1), trn_lbl)?,
+    let ds = linfa::Dataset::new(
+        ndarray::Array::from_shape_vec((trn_size, rows * cols), trn_img)?.mapv(|x| (x as f64) / 255.),
+        ndarray::Array::from_shape_vec((trn_size, 1), trn_lbl)?,
     );
 
     println!("prepared dataset");
 
     // reduce to 50 dimension without whitening
-    let ds = Pca::params(50)
+    let ds = linfa_reduction::Pca::params(50)
         .whiten(false)
         .fit(&ds)
         .unwrap()
@@ -62,7 +61,7 @@ fn main() -> Result<()> {
     println!("whitening");
 
     // calculate a two-dimensional embedding with Barnes-Hut t-SNE
-    let ds = TSneParams::embedding_size(2)
+    let ds = linfa_tsne::TSneParams::embedding_size(2)
         .perplexity(50.0)
         .approx_threshold(0.5)
         .max_iter(1000)
